24db3f6d07bc20efd7d2b535304948f3abe1e142
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2022-03-05 23:40:29 -0800

    add bestiary page

---
+++ shell-struggle/src/routes/bestiary.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import './bestiary.css';
+
+// TODO: figure this out
+temp_turtles = [
+  {
+    name: 'a', 
+    stat_1: 10, 
+    stat_2: 10, 
+    stat_3: 10, 
+    lore: 'LOREm ipsum', 
+    pic_url: 'https://images.squarespace-cdn.com/content/v1/5369465be4b0507a1fd05af0/1528837069483-LD1R6EJDDHBY8LBPVHIU/randall-ruiz-272502.jpg'
+  }, 
+  {
+    name: 'b', 
+    stat_1: 20, 
+    stat_2: 20, 
+    stat_3: 20, 
+    lore: 'b stands for better', 
+    pic_url: 'https://news.stanford.edu/wp-content/uploads/2021/04/Sea-Turtle.jpg'
+  }
+];
+
+class Bestiary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default Bestiary
