87eaa9843d989c957d0e1b6537bc7670be1aed50
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2022-03-06 01:48:56 -0800

    add header; also realize that header is not an mdb exclusive thing bruh :/ it's just some divs

--- shell-struggle/src/routes/bestiary.jsx
+++ shell-struggle/src/routes/bestiary.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 // import './bestiary.css';
-import { MDBCard, MDBCardBody, MDBCardFooter, MDBCardTitle, MDBCardText, MDBCardImage, MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  MDBCard, 
+  MDBCardBody, 
+  MDBCardFooter, 
+  MDBCardTitle, 
+  MDBCardText, 
+  MDBCardImage, 
+  MDBContainer, 
+  MDBRow, 
+  MDBCol, 
+  MDBNavbar, 
+  MDBNavbarItem, 
+  MDBNavbarLink, 
+  MDBNavbarNav, 
+  MDBNavbarToggler, 
+  MDBIcon
+} from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 /**
  * see: 
@@ -42,27 +58,64 @@ const many_turtles = [
   temp_turtles[1], 
   temp_turtles[0], 
   temp_turtles[1], 
-  temp_turtles[0], 
   temp_turtles[1], 
   temp_turtles[0], 
   temp_turtles[1], 
   temp_turtles[0], 
-  temp_turtles[1], 
   temp_turtles[0], 
   temp_turtles[1], 
   temp_turtles[0], 
   temp_turtles[1], 
+  temp_turtles[1], 
   temp_turtles[0], 
   temp_turtles[1], 
+  temp_turtles[0], 
   temp_turtles[0], 
   temp_turtles[1] 
 ]
 
-function make_card(turtle) {
+// TODO
+function temp_header() {
   return (
-    <MDBCol lg={true} style={{marginBottom: '1.5rem'}} className='col-3'>
+    <MDBNavbar expand='lg' light bgColor='white'>
+      <MDBContainer fluid>
+        <MDBNavbarToggler
+          aria-controls='navbarExample01'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <MDBIcon fas icon='bars' />
+          {/* icon does not show up */}
+        </MDBNavbarToggler>
+        <div className='collapse navbar-collapse' id='navbarExample01'>
+          <MDBNavbarNav right className='mb-2 mb-lg-0'>
+            <MDBNavbarItem active>
+              <MDBNavbarLink aria-current='page' href='#'>
+                Home (spoiler: doesnt work)
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Some Link</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Some Other Link</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Some Other Link 2</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </div>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
+
+function make_card(turtle) {
+  {/* currently causes duplicate keys but shouldnt matter later */}
+  return (
+    <MDBCol lg={true} style={{marginBottom: '1.5rem'}} className='col-3' key={turtle}>
       <MDBCard style={{ width: '18rem' }} className='h-100'>
-        <MDBCardImage variant='bottom' src={turtle.pic_url} />
+        <MDBCardImage position='top' src={turtle.pic_url} />
         <MDBCardBody>
           <MDBCardTitle>{turtle.name}</MDBCardTitle>
           <MDBCardText>
@@ -85,13 +138,31 @@ class Bestiary extends React.Component {
   render() {
     return (
       <div>
-        <h1>hi</h1>
-      
-        <MDBContainer className='container-fluid'>
-          <MDBRow>
-            {many_turtles.map(make_card)}
-          </MDBRow>
-        </MDBContainer>
+        <header>
+          {temp_header()}
+
+          <div
+            className='p-5 text-center bg-image'
+            style={{ backgroundImage: "url('http://www.thestoryoftexas.com/upload/images/events/movies/turtle-odyssey-banner-mobile.jpg')", height: 400 }}
+          >
+            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <div className='text-white'>
+                  <h1 className='mb-3'>Bestiary</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        
+        <div style={{ height: '1.5rem' }}/>
+        <div>
+          <MDBContainer className='container-fluid'>
+            <MDBRow>
+              {many_turtles.map(make_card)}
+            </MDBRow>
+          </MDBContainer>
+        </div>
       </div>
     );
   }
