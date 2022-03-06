41754c66c3581c84d41a1a0b221a7521e555abf2
Zane Witter <zanewitter@g.ucla.edu>
2022-03-05 16:17:14 -0800

    Functions to manipulate turtle collection
    You can now use getTurtles() and unlockTurtle() to get a user's
    array of turtles and set one of the elements in the array to true,
    respectively. In my mind, the collection of turtles should be
    stored as an array of booleans, with each index corresponding to
    a given turtle being unlocked/purchased for the user.

--- shell-struggle/src/database.js
+++ shell-struggle/src/database.js
@@ -7,6 +7,33 @@ async function getUserRef (username) {
     return userRef;
 } 
 
+async function getTurtles(user) {
+    const userRef = await getUserRef(user);
+    const userProfile = await getDoc(userRef);
+    if (userProfile.exists()) {
+        const userData = userProfile.data();
+        return userData.turtles;
+    } else {
+        console.log("ERROR in getTurtles(): user", user, "does not exist"); return false;
+    }
+}
+
+async function unlockTurtle(user, index) {
+    const userRef = await getUserRef(user);
+    let turtleArray = await getTurtles(user);
+    if (turtleArray === false) {return false}
+    turtleArray[index] = true;
+    const newTurtles = {
+        turtles: turtleArray
+    };
+    try {
+        await updateDoc(userRef, newTurtles);
+        return newTurtles;
+    } catch {
+        console.log("ERROR in unlockTurtle(): user", user, "did not exist"); return false;
+    }
+}
+
 async function getWallet (user) {
     const userRef = await getUserRef(user);
     const userProfile = await getDoc(userRef);
@@ -14,17 +41,16 @@ async function getWallet (user) {
         const userData = userProfile.data();
         return userData.wallet;
     } else {
-        console.log("ERROR in getWallet(): user", user, "did not exist");
-        return -1;
+        console.log("ERROR in getWallet(): user", user, "did not exist"); return false;
     }
 }
 
 async function incWallet (user, amount) {
-    const balance = await getWallet(user)
+    const balance = await getWallet(user);
+    if (balance === false) {return false;}
     const newAmount = balance + amount;
     if (newAmount < 0) {
-        console.log("ERROR in incWallet(): insufficient funcds")
-        return -1;
+        console.log("ERROR in incWallet(): insufficient funcds"); return false;
     }
     const userProfile = await getUserRef(user);
     const newWallet = {
@@ -33,12 +59,10 @@ async function incWallet (user, amount) {
     try {
         await updateDoc(userProfile, newWallet);
     } catch {
-        console.log("Error in incWallet(): user", user, "doesn't exist");
-        return -1;
+        console.log("Error in incWallet(): user", user, "doesn't exist"); return false;
     }
     return newAmount;
 }
 
-
-export { getWallet, incWallet }
+export { getTurtles, unlockTurtle, getWallet, incWallet }
 
