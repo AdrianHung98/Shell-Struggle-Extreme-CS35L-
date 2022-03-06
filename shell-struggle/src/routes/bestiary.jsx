04bfdb3d7cd0b3a6f997f462118000d6c16fefd3
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2022-03-06 00:54:57 -0800

    adjustments

--- shell-struggle/src/routes/bestiary.jsx
+++ shell-struggle/src/routes/bestiary.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 // import './bestiary.css';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // TODO: figure this out
 const temp_turtles = [
   {
     name: 'a', 
-    stat_1: 10, 
-    stat_2: 10, 
-    stat_3: 10, 
+    desc: 'first temp turtle', 
+    spd: 10, 
+    str: 10, 
+    int: 10, 
     lore: 'LOREm ipsum', 
     pic_url: 'https://images.squarespace-cdn.com/content/v1/5369465be4b0507a1fd05af0/1528837069483-LD1R6EJDDHBY8LBPVHIU/randall-ruiz-272502.jpg'
   }, 
   {
     name: 'b', 
-    stat_1: 20, 
-    stat_2: 20, 
-    stat_3: 20, 
+    desc: 'second temp turtle', 
+    spd: 20, 
+    str: 20, 
+    int: 20, 
     lore: 'b stands for better', 
     pic_url: 'https://news.stanford.edu/wp-content/uploads/2021/04/Sea-Turtle.jpg'
   }
@@ -46,16 +48,18 @@ const many_turtles = [
 
 function make_card(turtle) {
   return (
-    <Col>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={turtle.pic_url} style={{width: '16rem'}}/>
+    <Col lg={true} style={{marginBottom: '1.5rem'}}>
+      <Card style={{ width: '18rem' }} className='h-100'>
+        <Card.Img variant='bottom' src={turtle.pic_url} style={{width: '18rem'}}/>
         <Card.Body>
           <Card.Title>{turtle.name}</Card.Title>
           <Card.Text>
-            Lore: {turtle.lore}
+            {turtle.desc}
           </Card.Text>
-          <Button variant="primary">do we need this button</Button>
         </Card.Body>
+        <Card.Footer>
+          SPD {turtle.spd} / STR {turtle.str} / INT {turtle.int}
+        </Card.Footer>
       </Card>
     </Col>
   );
@@ -71,8 +75,8 @@ class Bestiary extends React.Component {
       <div>
         <h1>hi</h1>
       
-        <Container>
-          <Row md={4}>
+        <Container class='row-cols-4'>
+          <Row>
             {many_turtles.map(make_card)}
           </Row>
         </Container>
