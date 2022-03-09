42a973a4fffb4ccce59a2957d8001ae89e245e52
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2022-03-09 12:06:12 -0800

    add 'press enter to search' to user search bar

--- shell-struggle/src/navbar.js
+++ shell-struggle/src/navbar.js
@@ -24,7 +24,18 @@ function Navbar(props) {
           </ul>
       
           <div className="input-group m-1 w-25">
-            <input id="searchBar" type="search" className="form-control rounded col-2" placeholder="Search for User" aria-label="Search for User" aria-describedby="search-addon" />
+            <input id="searchBar" type="search" className="form-control rounded col-2" placeholder="Search for User" aria-label="Search for User" aria-describedby="search-addon" onKeyPress={ async e => {
+              if (e.key === 'Enter') {
+                const query = document.getElementById('searchBar').value;
+                if (query === "") return;
+                const uid = await getUIDByUsername(query);
+                if (!uid) {
+                  alert(`User "${query}" not found.`);
+                  return;
+                }
+                window.location.href = `/profile/${uid}`;
+              }
+            } }/>
             <button type="button" className="btn btn-outline-primary" onClick={ 
               async () => {
                 const query = document.getElementById('searchBar').value;
