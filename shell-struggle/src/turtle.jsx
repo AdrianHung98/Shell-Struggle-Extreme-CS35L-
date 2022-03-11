025ceb03045f0510b8e2ee6a1d4e81e7bf4c3f74
Zane Witter <zanewitter@g.ucla.edu>
2022-03-10 19:13:24 -0800

    (Collab with Jon) Players choose at same time
    Both players select a move, and then we will add what to do
    with their selections and play them out on both screens.

--- shell-struggle/src/turtle.jsx
+++ shell-struggle/src/turtle.jsx
@@ -2,22 +2,33 @@
 import './turtle.css';
 
 function Turtle(props) {
+    const imgWidth = 300;
+    const percentage = 100*(props.health / props.maxHealth);
+    const percentageWidth = percentage.toString() + "%"
+    const displayHealth = (percentage === 0)? "hidden": "visible";
+    //don't display any health if no health
+
     return(
         <div>
             <style>
             </style>
-            <div className="turtle-display">            
+            <div className="TurtleDisplay">            
             <img 
                 src={props.image} 
-                width="128"
-                height="128"
+                width={imgWidth}
+                height="300px%"
                 alt=""
                 />
-            <ul>
-                <li>HP: {props.health}</li>
-                <li>INT: {props.intelligence}</li>
-                <li>STR: {props.strength}</li>
-            </ul>
+            <div>
+                <div className="HealthDisplay"><div>HP:</div>
+                        <div className="HealthBar">
+                            <div className="PercentageBar" style={{"visibility": displayHealth,"width": percentageWidth}}></div>
+                        </div>
+                        <div style={{float: 'right'}}>{props.health}/{props.maxHealth}</div>
+                        </div>
+                
+                <div style={{"display" : "flex"}}><div style={{"flex": 1}}>HP: {props.maxHealth}</div><div style={{"flex": 1}}>INT: {props.intelligence}</div><div style={{"flex": 1}}>STR: {props.strength}</div></div>
+            </div>
             </div>
         </div>
     );
